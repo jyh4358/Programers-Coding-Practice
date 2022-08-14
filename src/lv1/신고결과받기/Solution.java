f1082349fb5255ff8144bfc2c9c5157015c55460
jyh4358 <jyh4358@gmail.com>
2022-08-15 02:16:08 +0900

    [카카오] 신고결과받기

--- src/lv1/신고결과받기/Solution.java
+++ src/lv1/신고결과받기/Solution.java
@@ -1,35 +1,42 @@
 package lv1.신고결과받기;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
+        // 1. 중복 제거
+        HashSet<String> reportSet = new HashSet<String>();
+        for (String rep : report)
+            reportSet.add(rep);
 
-        HashMap<String, HashSet<String>> reportedMap = new HashMap<>();
+        // 2. report에서 각 사람이 신고당한 횟수를 countHash으로 정의하기
+        HashMap<String, ArrayList<String>> notifyListHash = new HashMap<>();
+        for (String rep : reportSet){
+            int blankIdx = rep.indexOf(" ");
+            String reporter = rep.substring(0, blankIdx);
+            String reportee = rep.substring(blankIdx + 1);
 
+            ArrayList<String> reporterList = notifyListHash.getOrDefault(reportee, null);
+            if(reporterList == null) reporterList = new ArrayList<>();
 
-        for (String re : report) {
-            String[] str = re.split(" ");
-
-            HashSet<String> reportSet = reportedMap.getOrDefault(str[1], new HashSet<String>());
-            reportSet.add(str[0]);
-            reportedMap.put(str[1], reportSet);
+            reporterList.add(reporter);
+            notifyListHash.put(reportee, reporterList);
         }
 
-        for (String key : reportedMap.keySet()) {
-            if (reportedMap.get(key).size() > 1) {
+        // 3. notifyListHash를 기반으로 reportHash만들기
+        HashMap<String, Integer> reporterHash = new HashMap<>();
+        for (ArrayList<String> notifyList : notifyListHash.values())
+            if (notifyList.size() >= k)
+                for(String reporter : notifyList)
+                    reporterHash.put(reporter, reporterHash.getOrDefault(reporter, 0) + 1);
 
-            }
-        }
-
+        // 4. reporterHash 정보를 answer에 옮겨담기
+        for (int i = 0; i < id_list.length; i++)
+            answer[i] = reporterHash.getOrDefault(id_list[i], 0);
 
         return answer;
     }
 
-    class Airline {
-
-    }
 }
+
