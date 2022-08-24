fec63de4c32d7bc7993a1ae09fb882d210c12a20
jyh4358 <jyh4358@gmail.com>
2022-08-24 22:50:29 +0900

    [연습문제] 가운데 글자 가져오기

---
+++ src/lv1/가운데글자가져오기/Solution.java
@@ -0,0 +1,16 @@
+package lv1.가운데글자가져오기;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        if (s.length() % 2 == 1) {
+            answer = s.substring(s.length() / 2, s.length() / 2 + 1);
+        } else {
+            answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+        }
+
+
+        return answer;
+    }
+}
