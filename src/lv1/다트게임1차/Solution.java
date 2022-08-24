e4318227fbe1920452e40b79827e1f196b8b103f
jyh4358 <jyh4358@gmail.com>
2022-08-24 23:11:32 +0900

    [카카오] 1차 다트 게임

--- src/lv1/다트게임1차/Solution.java
+++ src/lv1/다트게임1차/Solution.java
@@ -1,22 +1,46 @@
-package lv1.다트게임1차;
-
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
+        int[] dart = new int[3];
 
-        String dart = "";
-        char ch;
-        for (int i = 0; i < dartResult.length(); i++) {
-            ch = dartResult.charAt(i);
-            if (ch >= '0' && ch <= '9') {
-                dart += ch;
-                continue;
+        int n=0,idx=0;
+        String numstr="";
+
+        for(int i=0;i<dartResult.length();i++){
+            char c = dartResult.charAt(i);
+
+            //숫자일 때
+            if(c>='0'&&c<='9'){
+                numstr+=String.valueOf(c);
+
             }
-            if (ch == '*') {
-
+            //보너스일 때
+            else if(c=='S'||c=='D'||c=='T'){
+                n=Integer.parseInt(numstr);
+                if(c=='S'){
+                    dart[idx++]=(int)Math.pow(n,1);
+                }
+                else if(c=='D'){
+                    dart[idx++]=(int)Math.pow(n,2);
+                }
+                else{
+                    dart[idx++]=(int)Math.pow(n,3);
+                }
+                numstr="";
+            }
+            //옵션일 때
+            else {
+                if(c=='*'){
+                    dart[idx-1]*=2;
+                    if(idx-2>=0) dart[idx-2]*=2;
+                }
+                else {
+                    dart[idx-1]*=(-1);
+                }
             }
         }
 
+        answer=dart[0]+dart[1]+dart[2];
 
         return answer;
     }
