cb482667d71410181e69d561d7206ec69201672d
jyh4358 <jyh4358@gmail.com>
2022-10-09 23:36:13 +0900

    [스택/큐] 올바른 괄호

---
+++ src/lv2/올바른괄호/Solution.java
@@ -0,0 +1,27 @@
+package lv2.올바른괄호;
+
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                st.push('(');
+            }else{
+                if(st.isEmpty()){
+                    answer = false;
+                    break;
+                }else{
+                    st.pop();
+                }
+            }
+        }
+        if(st.size() > 0)
+            return false;
+
+        return answer;
+    }
+}
+
