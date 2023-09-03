4815fce80cd83c0d7ac0370e6a42dbb327fac3eb
yoonho <jyh4358@gmail.com>
2023-09-03 23:19:07 +0900

    이전에 풀었던 프로그래머스 문제 삭제

--- src/lv1/짝수와홀수/Solution.java
+++
@@ -1,7 +0,0 @@
-package lv1.짝수와홀수;
-
-class Solution {
-    public String solution(int num) {
-        return num % 2 == 0 ? "Even" : "Odd";
-    }
-}
--- src/lv1/평균구하기/Solution.java
+++
@@ -1,9 +0,0 @@
-package lv1.평균구하기;
-
-import java.util.Arrays;
-
-class Solution {
-    public double solution(int[] arr) {
-        return Arrays.stream(arr).average().getAsDouble();
-    }
-}
